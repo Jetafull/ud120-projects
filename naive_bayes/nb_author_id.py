a752b01f2fd6f263a4136371b5489af1fecc6bdd
Jetafull <jetafull@gmail.com>
2017-04-04 15:15:46 +0900

    Use naive bayes to predict who is the author of the email.

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -26,8 +26,11 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
+cls = GaussianNB()
+pred = cls.fit(features_train, labels_train).predict(features_test)
 
+float(np.sum(pred == labels_test)) / len(labels_test)
 
 #########################################################
-
-
